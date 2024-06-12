e90c3deaf3d24cae3050dc01e78d3c79d262e522
chenxuanfeng <chenxuanfeng@meituan.com>
2024-06-12 21:44:26 +0800

    feat: up

--- webpack/webpack.pro.config.js
+++ webpack/webpack.pro.config.js
@@ -10,7 +10,26 @@ config.module.rules.push(
 		test: /\.(es6|jsx|js|ts|tsx)$/,
 		exclude: /node_modules/,
 		use: [
-			'babel-loader',
+			{
+				loader: 'babel-loader',
+				options: {
+					presets: [
+						'@babel/preset-env', // 用于将现代 JavaScript 转换为兼容旧版环境的代码
+						[
+							"@babel/preset-react",
+							{
+							  "runtime": "automatic" // 对于 React 17 或更高版本
+							}
+						],
+						'@babel/preset-typescript', // 用于处理 TypeScript
+					],
+					plugins: [
+						// 这里可以添加 Babel 插件，例如：
+						'@babel/plugin-proposal-class-properties', // 用于支持类属性语法
+						'@babel/plugin-transform-runtime', // 避免重复引入辅助代码
+					]
+				},
+			},
 			{
 				loader: 'ts-loader',
 				options: {
