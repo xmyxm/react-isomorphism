e65c2980a2aa17fbb1caf1b9e6e9e9148bc96b88
chenxuanfeng <chenxuanfeng@meituan.com>
2024-06-30 11:42:14 +0800

    feat: up

--- src/app/component/weather/index.tsx
+++ src/app/component/weather/index.tsx
@@ -1,12 +1,58 @@
+/* eslint-disable camelcase */
 import { ReactElement } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { IndexModel } from '../../store/models/base/modelType'
+import { WeatherStateType } from '../../store/models/base/type/weatherInfoType'
 import './index.less'
 
-export default function Weather(): ReactElement {
-	const weatherState = useSelector((state: IndexModel) => state.weather)
+export default function Weather(): ReactElement | null {
+	// @ts-ignore
+	const weatherState: WeatherStateType = useSelector((state: IndexModel) => state.weather)
 	// const menuState = useSelector((state: IndexModel) => state.tripMenu)
 	// const dispatch = useDispatch<Dispatch>()
 
-	return <div className="weather">{JSON.stringify(weatherState.weatherInfo || {})}</div>
+	if (!weatherState.weatherInfo) return null
+
+	const {
+		station: { city },
+		publish_time,
+		weather: { temperature, rain, airpressure, humidity },
+		wind: { direct, power },
+	} = weatherState.weatherInfo
+
+	return (
+		<div className="weather">
+			<div className="title">
+				<div className="cityname">
+					<i className="locicon"></i>
+					{city}
+				</div>
+				<div className="uptime">{publish_time}</div>
+			</div>
+			<div className="temperature">{temperature}</div>
+			<div className="detaillist">
+				<div>
+					<img className="icon" />
+					<div className="text">降水量</div>
+					<div className="text">{rain}mm</div>
+				</div>
+				<div>
+					<div className="icon"></div>
+					<div className="text">气压</div>
+					<div className="text">{airpressure}hpa</div>
+				</div>
+				<div>
+					<div className="icon"></div>
+					<div className="text">湿度</div>
+					<div className="text">{humidity}%</div>
+				</div>
+				<div>
+					<div className="icon"></div>
+					<div className="text">{direct}</div>
+					<div className="text">{power}</div>
+				</div>
+			</div>
+			{/* {JSON.stringify(weatherState.weatherInfo || {})} */}
+		</div>
+	)
 }
